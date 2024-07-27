10e165fcceac5b6145919624fa2034b3f7a26860
Ozgun OZ <ozgunn.oz@gmail.com>
2024-07-27 17:14:07 +0200

    use bean types from plugin configuration for generating provider classes

--- src/it/example/plugin/src/main/java/com/homeofthewizard/plugin/MeetingMojo.java
+++ src/it/example/plugin/src/main/java/com/homeofthewizard/plugin/MeetingMojo.java
@@ -3,19 +3,19 @@ package com.homeofthewizard.plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
-import com.homeofthewizard.friends.MyFriend;
-import com.homeofthewizard.dinerlib.Pizza;
+import com.homeofthewizard.friends.MyHelloer;
+import com.homeofthewizard.dinerlib.Diner;
 
 import javax.inject.Inject;
 
 @Mojo(name = "meeting", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class MeetingMojo extends AbstractMojo {
 
-    private final MyFriend myFriend;
-    private final Pizza diner;
+    private final MyHelloer myFriend;
+    private final Diner diner;
 
     @Inject
-    public MeetingMojo(MyFriend myFriend, Pizza diner) {
+    public MeetingMojo(MyHelloer myFriend, Diner diner) {
 
         this.myFriend = myFriend;
         this.diner = diner;
